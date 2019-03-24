35c96442a19eed2f23f61f0671501566d907e691
Dank5 <lewis_danks@hotmail.co.uk>
2019-03-24 23:07:32 +0000

    Added simple html and stylesheet as well as p5 canvas

---
+++ game.js
@@ -0,0 +1,19 @@
+// Store the background colour
+bgColour = "#006400";
+
+// Function to setup game variables
+function setup() {
+	
+	// Create the canvas and set the parent
+	cnv = createCanvas(800,800);
+	cnv.parent("gameContainer");
+	
+}
+
+// Game loop
+function draw() {
+	
+	// Clear the background
+	background(bgColour);
+	
+}
